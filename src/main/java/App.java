05e2dd8eefdd04f78cd6b39d93a920e879607358
Sergo Fan <sergei.fonarev@yandex.ru>
2024-03-21 14:01:58 +0300

    AppTest win

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,24 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        boolean comparison = bean == bean2;
+        System.out.println(comparison);
+
+        Cat beanCat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getValue());
+
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat2.getValue());
+
+        boolean comparison2 = beanCat == beanCat2;
+        System.out.println(comparison2);
+
     }
 }
